8b2c2c5d6a003ed6be44bba6483588052adbc797
Michal <khakaimic4@gmail.com>
2023-03-17 11:56:18 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function  saturdayFun(doing = "roller-skate"){
+   return `This Saturday, I want to ${doing}!`
+}
+    
+    
+
+
+// 2
+function mondayWork (work = "go to the office"){
+return `This Monday, I will ${work}.`
+}
+
+
+
+
+//3
+ function wrapAdjective(string ="*") {
+return function(parameter="a hard worker!"){
+return `You are ${string}${parameter}${string}!`
+}
+
+}
